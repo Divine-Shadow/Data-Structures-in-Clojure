d8a34eda8940b5eeae0b39f61f83d13f3f6995d6
wmolchan <wmolchan@hawk.iit.edu>
2013-11-23 14:16:51 -0600

    did bst test cases

--- bst/spec/bst/core_spec.clj
+++ bst/spec/bst/core_spec.clj
@@ -14,4 +14,44 @@
           (it "should have some tests."
               (should true)))
 
+
+(describe "add"
+
+          (it "should not increment size" (should= 2 (size (add (add (make-tree) 2 4) 4 "apple"))))
+          (it "should not increment size" (should= 1 (size (add (make-tree) 4 "apple"))))
+          (it "should not swap left and right" (should= (:left (:root (add (add (make-tree) 4 "apple") 3 "bannana")))  (:left (:root (add (add (add (make-tree) 4 "apple") 3 "bannana") 9 "pear")))))
+)
+
+(describe "find-key" 
+          (it "should check both sides" (should (find  9 (add (add (add (make-tree) 4 "apple") 3 "bannana") 9 "pear"))))
+          (it "should check both sides" (should (find-key  "pear" (add (add (add (make-tree) 4 "apple") 3 "bannana") 9 "pear"))))
+ (it "should check both sides" (should (find  4 (add (add (add (make-tree) 4 "apple") 3 "bannana") 9 "pear"))))
+          (it "should check both sides" (should (find-key  "bannana" (add (add (add (make-tree) 4 "apple") 3 "bannana") 9 "pear"))))
+)
+
+(describe "delete"
+
+          (it "should decrement size" (should= 2 (size (delete (add (add (add (make-tree) 4 "apple") 3 "bannana") 9 "pear") 3) ) ))
+          (it "should delete value" (should=  (delete (add (add (add (make-tree) 4 "apple") 3 "bannana") 9 "pear") 4) (add (add (make-tree)  3 "bannana") 9 "pear")            ))
+         
+)
+
+
+(describe "delete-value"
+
+          (it "should decrement size" (should= 2 (size (delete-value (add (add (add (make-tree) 4 "apple") 3 "bannana") 9 "pear") "apple") ) ))
+          (it "should delete value" (should=  (delete-value (add (add (add (make-tree) 4 "apple") 3 "bannana") 9 "pear") "apple") (add (add (make-tree)  3 "bannana") 9 "pear")            ))
+         
+)
+
+
+(describe "map-tree" 
+
+(it "should map values" (should= (map-tree (add (add (add (make-tree) 4 1) 3 2) 9 3) inc) (add (add (add (make-tree) 4 2) 3 3) 9 4)))
+
+
+)
+
+
+
 (run-specs)
