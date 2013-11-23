8c944b4989eac1b78b07512253589f07749a726a
wmolchan <wmolchan@hawk.iit.edu>
2013-11-23 15:23:21 -0600

    traversal edit

--- traversal-lab/spec/traversals/core_spec.clj
+++ traversal-lab/spec/traversals/core_spec.clj
@@ -8,9 +8,34 @@
 ;;
 ;; We are going to use [spelj](https://github.com/slagyr/speclj) for our tests.
 
-
+(def basecase (reduce add nil '(8 4 3 7 9 10 11)))
 (describe "The spec file"
           (it "should have some tests."
-              (should false)))
+              (should true)))
+
+
+(describe "preorder swap"
+(it "should not swap" (should= (preorder basecase) '( 8 4 3 7 9 10 11))) 
+)
+
+
+(describe "inorder"
+(it "should not swap" (should= (inorder basecase) '(3 4 7 8 10 9 11))) 
+)
+
+
+(describe "postorder"
+(it "should not swap" (should= (postorder basecase) '(3 7 4 10 11 9 8))) 
+)
+
+(describe "frontier"
+(it "should not swap" (should= (preorder basecase) '(3 7 10 11))) 
+)
+
+
+(describe "levelorder"
+(it "should not swap" (should= (levelorder basecase) '(8 4 9 2 7 10 11))) 
+)
+
 
 (run-specs)
